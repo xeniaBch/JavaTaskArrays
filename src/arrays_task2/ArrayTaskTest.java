23024f131d97baee669be1444b905808466828f4
xeniaBch <kseniabekhtgold@gmail.com>
2022-09-23 09:53:51 +0200

    HW changes

--- src/arrays_task2/ArrayTaskTest.java
+++ src/arrays_task2/ArrayTaskTest.java
@@ -3,14 +3,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ArrayTaskTest {
-    int[] ar = {1, 3 ,2, 2, 3 ,0};
-    int[] res = {2,3};
 
 
     @Test
     public void isRepeatedTrue(){
+        int[] ar = {1, 3 ,2, 2, 3 ,0};
+        int[] res = {2,3};
         Assert.assertArrayEquals(ArraysTask.repeatedNumbers(ar), res);
     }
 
+    @Test
+    public void isRepeated1True(){
+        int[] ar = {0, 0};
+        int[] res = {0};
+        Assert.assertArrayEquals(ArraysTask.repeatedNumbers(ar), res);
+    }
 
+    @Test
+    public void isRepeated2True(){
+        int[] ar = {1, 7, 8, 8, 7, 7, 8, 9};
+        int[] res = {8, 7};
+        Assert.assertArrayEquals(ArraysTask.repeatedNumbers(ar), res);
+    }
 }
