0989c7e86e315383d670df4e2f65e22dd7de6d2f
xeniaBch <kseniabekhtgold@gmail.com>
2022-09-23 18:01:15 +0200

    HW changes

--- src/primeNumbers_task1/PrimeNumbers.java
+++ src/primeNumbers_task1/PrimeNumbers.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class PrimeNumbers {
 
+    //it is slower on big amount of numbers
     public static void printPrime(){
         boolean isPrime;
         for (int n = 2; n <= 100; n++) {
@@ -23,32 +24,48 @@ public class PrimeNumbers {
         System.out.println();
     }
 
+    //it is faster on big amount of numbers
+    public static void printPrimeWithList(int number) {
 
-        public static void printPrime2(int n) {
-            List<Integer> list = new ArrayList<Integer>();
+        // there are [2, 3, 5, 7] for numbers less than 100
+        List<Integer> listOfPrimeDivisors = createListOfPrimeDivisors((int) Math.sqrt(number));
 
-            for (int i = 2; i <= n; i++) {
-                list.add(i);
-            }
-            //System.out.println("ArrayList : " + list.toString());
+        boolean isPrime;
+        for (int i = 2; i <= number; i++) {
+            isPrime = true;
 
-
-            for (int i = 0; list.get(i) * list.get(i) <= n; i++) {
-                for (int j = i + 1; j < list.size(); j++) {
-                    if (list.get(j) % list.get(i) == 0) {
-                        list.remove(j);
-                    }
+            for (int item : listOfPrimeDivisors) {
+                if (i % item == 0 && i != item) {
+                    isPrime = false;
+                    break;
                 }
             }
-            System.out.println(list.toString());
-
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
+    }
 
+    public static List<Integer> createListOfPrimeDivisors(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 2; i <= n; i++) {
+            list.add(i);
+        }
+        for (int i = 0; list.get(i) * list.get(i) <= n; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) % list.get(i) == 0) {
+                    list.remove(j);
+                }
+            }
+        }
+        return list;
 
+    }
 
 
     public static void main(String[] args) {
         printPrime();
-        printPrime2(100);
+        printPrimeWithList(100);
     }
 }
